f8fa2920da5bb86f0ca401a671e03161466bb015
Ева <eva56m@yandex.ru>
2025-02-24 12:22:52 +0500

    задача 5

--- api_file.py
+++ api_file.py
@@ -2,12 +2,12 @@ import requests
 from io import BytesIO
 
 
-def show_map(search_params=None, server=None):
+def show_map(params=None, server=None):
     # key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
     #
     # url = f"https://static-maps.yandex.ru/v1?ll={lon},{lat}&spn={spn[0]},{spn[1]}&apikey={key}"
 
-    response = requests.get(server, params=search_params)
+    response = requests.get(server, params=params)
 
     if response.status_code == 200:
         image = BytesIO(response.content)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from api_file import show_map
 from TimurTextInput import TextBox
 from button import Button
+import requests
 
 
 def change_theme():
@@ -12,6 +13,23 @@ def change_theme():
         params['theme'] = 'light'
 
 
+def find_object(search_server=None, search_params=None):
+    global params, server
+    response = requests.get(search_server, params=search_params)
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    params['ll'] = ",".join((toponym_longitude, toponym_lattitude))
+    return show_map(params=params, server=server)
+
+
 if __name__ == "__main__":
     pygame.init()
     size = (600, 500)
@@ -20,25 +38,35 @@ if __name__ == "__main__":
 
     screen = pygame.display.set_mode(size)
 
-    search_server = 'https://static-maps.yandex.ru/v1'
+    server = 'https://static-maps.yandex.ru/v1'
+
+    search_server = "http://geocode-maps.yandex.ru/1.x/"
 
     params = {'ll': ",".join((w, h)),
               'apikey': "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13",
               "spn": ",".join(spn),
               'theme': 'light'}
 
-    img_data = show_map(search_params=params, server=search_server)
+    search_params = {
+        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+        "geocode": '',
+        "format": "json"}
+
+    img_data = show_map(params=params, server=server)
     if img_data:
         img = pygame.image.load(img_data)
     else:
         print("Не удалось загрузить карту")
         pygame.quit()
 
-    place_input = TextBox(5, 455, 100, 40, 20)
+    place_input = TextBox(5, 455, 200, 40, 20)
 
-    search_btn = Button(110, 455, 'Искать')
+    search_btn = Button(210, 455, 'Искать')
 
-    theme_btn = Button(210, 455, 'Сменить тему')
+    theme_btn = Button(310, 455, 'Сменить тему')
+
+    point = None
+    point_coords = None
 
     running = True
     while running:
@@ -48,11 +76,20 @@ if __name__ == "__main__":
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if search_btn.is_hovered:
-                        pass
+                        search_params["geocode"] = place_input.text
+                        img = pygame.image.load(find_object(search_server=search_server, search_params=search_params))
+                        point = pygame.transform.scale(pygame.image.load('Map-Pin.png'), (10, 20))
+                        point_coords = (size[0] // 2 - 5, (size[1] - 50) // 2 - 20)
                     if theme_btn.is_hovered:
                         change_theme()
-                        img = pygame.image.load(show_map(search_params=params, server=search_server))
+                        img = pygame.image.load(show_map(params=params, server=server))
             place_input.input(event)
+
+        if pygame.key.get_pressed()[pygame.K_RETURN]:
+            search_params["geocode"] = place_input.text
+            img = pygame.image.load(find_object(search_server=search_server, search_params=search_params))
+            point = pygame.transform.scale(pygame.image.load('Map-Pin.png'), (10, 20))
+            point_coords = (size[0] // 2 - 5, (size[1] - 50) // 2 - 20)
 
         screen.fill((255, 255, 255))
         screen.blit(img, (0, 0))
@@ -61,6 +98,8 @@ if __name__ == "__main__":
         theme_btn.draw(screen)
         search_btn.update(pygame.mouse.get_pos())
         theme_btn.update(pygame.mouse.get_pos())
+        if point is not None:
+            screen.blit(point, point_coords)
         pygame.display.flip()
 
     pygame.quit()
